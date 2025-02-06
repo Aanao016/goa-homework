2310e6ee8e826053f6240e6a2476ff0970135744
Aanao016 <tsagareishviliano38@gmail.com>
2025-02-06 23:51:20 +0400

    Commit

---
+++ Day 01/Homework/homework.py
@@ -0,0 +1,6 @@
+name="anano"
+username="tssagareihvili"
+age=15
+height="1.70"
+weight=50
+print("chemi sakhelia",name,username, "chemi asakia",age, "chemi simaghlea",height,"chemi wonaa",weight,)  
