dc794254377daa675bddbf1f699255d7929968a3
Aanao016 <tsagareishviliano38@gmail.com>
2025-03-09 22:24:06 +0400

    comit

--- Day 03/homework/hw.py
+++ Day 03/homework/hw.py
@@ -1,196 +1,29 @@
-from turtle import *
+#თქვენი სიტყვებით ახსენით, თუ რა არის დეკლარაცია, ინიციალიზება და რეინიციალიზება.
+#დეკლარაცია – ცვლადის გამოცხადება პროგრამაში, ანუ მისი შექმნა.
+#ინიციალიზება – ცვლადის პირველადი მნიშვნელობის მინიჭება.
+#რეინიციალიზება – ცვლადის უკვე მინიჭებული მნიშვნელობის შეცვლა ახალით.
 
-# we want to peint a tower
+#შექმენით ცვლადი, შეუცვალეთ მას მნიშვნელობა 3-ჯერ. თითოეული მნიშვნელობის შეცვლის შემდეგ დაბეჭდეთ ეს ცვლადი.
+x = 5  
+print(x)
 
-# step 1: draw a square
-speed(30)
-width(2)
-color("black")
-begin_fill()
-forward(300)
-right(90)
+x = 10  
+print(x)
 
+x = 15  
+print(x)
 
-forward(300)
-right(90)
+# შექმენით 5 ცვლადი, სადაც შეინახავთ რიცხვებს. დაბეჭდეთ მათი ჯამი print ფუნქციის გამოყენებით.
+a = 4
+b = 7
+c = 12
+d = 9
+e = 18
+# მართკუთხედის პერიმეტრი:
+#შექმენით ორი ცვლადი length და width(მართკუთხედის გვერდები), მიანიჭეთ მნიშვნელობებად 20 და 10. გამოითვალეთ მართკუთხედის პერიმეტრი და დაბეჭდეთ.print(a + b + c + d + e)
 
-forward(300)
-right(90)
- 
-end_fill()
-forward(300) 
-right(90)
-penup()
-left(90)
-pendown()
-color("grey")
-begin_fill()
-forward(30)
-right(90)
+length = 20
+width = 10
 
-forward(30)
-right(90)
-
-forward(20)
-left(90)
-
-forward(30)
-left(90)
-
-forward(20)
-right(90)
-
-forward(30)
-right(90)
-
-forward(20)
-left(90)
-
-forward(30)
-left(90)
-
-forward(20)
-right(90)
-
-forward(30)
-right(90)
-
-forward(20)
-left(90)
-
-forward(30)
-left(90)
-
-forward(20)
-right(90)
-
-forward(30)
-right(90)
-
-
-forward(20)
-left(90)
-
-forward(30)
-left(90)
-
-forward(20)
-right(90)
-
-forward(30)
-right(90)
-
-forward(20)
-left(90)
-
-forward(30)
-right(90)
-forward(10)
-
-end_fill()
-
-penup()
-goto(50, 100)
-forward(150)
-left(90)
-pendown()
-
-color("blue")
-begin_fill()
-forward(80)
-right(90)
-
-forward(80)
-right(90)
-
-forward(80)
-right(90)
-
-forward(80)
-right(90)
-end_fill()
-
-
-color("blue")
-begin_fill()
-penup()
-forward(150)
-right(90)
-pendown()
-
-forward(80)
-left(90)
-
-forward(80)
-left(90)
-
-forward(80)
-left(90)
-forward(80)
-end_fill()
-
-#red of the window
-
-
-#peint a door
-
-
-penup()
-color("yellow")
-begin_fill()
-left(90)
-forward(80)
-left(90)
-
-
-forward(40)
-right(90)
-
-forward(40)
-pendown()
-
-forward(130)
-right(90)
-
-forward(170)
-right(90)
-
-forward(130)
-right(90)
-
-forward(30)
-right(90)
-
-forward(30)
-left(90)
-
-forward(30)
-left(90)
-
-forward(30)
-right(90)
-
-forward(30)
-right(90)
-
-forward(30)
-left(90)
-
-forward(30)
-left(90)
-
-forward(30)
-right(90)
-
-forward(30)
-right(90)
-
-forward(30)
-left(90)
-
-forward(20)
-penup()
-left(90)
-
-
-exitonclick()
+perimeter = 2 * (length + width)
+print(perimeter)
