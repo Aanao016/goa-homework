92bc9f72fb626db8afa6d7c23f4871809aac5970
Anao016 <tsagareishviliano38@gmail.com>
2025-04-02 20:49:37 +0400

    commit

--- Day 14/homework/hw.py
+++ Day 14/homework/hw.py
@@ -26,3 +26,48 @@ if "apple" == "apple" and "banana" == "banana":
     print("Both strings are equal")
 
 
+
+if 5 > 3 or 2 > 7:
+    print("One of the conditions is True")
+
+if 10 > 5 or 3 < 7:
+    print("Both conditions are True")
+
+if 1 > 3 or 4 > 2:
+    print("This will print because the second condition is True")
+
+if 1 > 3 or 2 > 7:
+    print("This will not print")
+
+
+
+if not (5 > 3):
+    print("This will not print")
+
+if not (3 > 5):
+    print("This will print")
+
+if not ("apple" != "banana"):
+    print("This will not print")
+
+if not (7 == 7):
+    print("This will not print")
+
+
+
+
+
+if (5 > 3) != (7 < 2):
+    print("Only one condition is True")
+
+
+if (5 > 3) != (2 < 7):
+    print("This will not print")
+
+
+if (2 > 3) != (4 > 7):
+    print("This will not print either")
+
+
+if (10 == 10) != (7 > 8):
+    print("This will print because only one is True")
